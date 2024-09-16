0c7492d1fc3458b33e8f5db7f5489cfafc22c9e3
João Pedro <joaoplay16@gmail.com>
2024-09-16 20:12:51 -0300

    Create all notification channels at app start up

--- app/src/main/java/com/playlab/workmanagerapp/DownloadApplication.kt
+++ app/src/main/java/com/playlab/workmanagerapp/DownloadApplication.kt
@@ -4,19 +4,40 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import com.playlab.workmanagerapp.NotificationChannelsIds.ALARM_MANAGER_CHANNEL_ID
+import com.playlab.workmanagerapp.NotificationChannelsIds.DOWNLOAD_CHANNEL_ID
+import com.playlab.workmanagerapp.NotificationChannelsIds.JOB_SCHEDULER_CHANNEL_ID
 
-class DownloadApplication: Application() {
+class DownloadApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val chanel = NotificationChannel(
-                "download_channel",
+                DOWNLOAD_CHANNEL_ID,
                 "File download",
                 NotificationManager.IMPORTANCE_HIGH
             )
 
+            val alarmManagerChannel = NotificationChannel(
+                ALARM_MANAGER_CHANNEL_ID,
+                "Alarm Manager Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val jobSchedulerChannel = NotificationChannel(
+                JOB_SCHEDULER_CHANNEL_ID,
+                "Job Scheduler channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
             val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(chanel)
+            notificationManager.createNotificationChannels(
+                listOf(
+                    chanel,
+                    alarmManagerChannel,
+                    jobSchedulerChannel
+                )
+            )
         }
     }
 }
