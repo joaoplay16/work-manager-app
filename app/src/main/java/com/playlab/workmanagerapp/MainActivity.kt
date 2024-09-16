026ba5603b881d007d789be98b7e257d375fa9fe
João Pedro <joaoplay16@gmail.com>
2024-09-16 20:21:30 -0300

    Uses all periodic notifications methods

--- app/src/main/java/com/playlab/workmanagerapp/MainActivity.kt
+++ app/src/main/java/com/playlab/workmanagerapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.playlab.workmanagerapp
 
+import NotificationJobScheduler
+import NotificationWorker
 import android.os.Bundle
 import android.util.Log
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.derivedStateOf
@@ -17,15 +23,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
-import androidx.work.*
-import coil.compose.AsyncImage
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.playlab.workmanagerapp.ui.theme.WorkManagerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        NotificationWorker.scheduleWork(applicationContext)
+
+        NotificationSchedulerAlarmManager.scheduleNotification(applicationContext)
+
+        NotificationJobScheduler.scheduleNotification(
+            applicationContext,
+            15 * 60 * 1000
+        )
+
         val downloadRequest = OneTimeWorkRequestBuilder<DownloadWorker>()
             .setConstraints(
                 Constraints.Builder()
@@ -46,14 +65,21 @@ class MainActivity : ComponentActivity() {
                     .observeAsState()
                     .value
 
-                val downLoadInfo = remember(key1 = workInfos){
-                    workInfos?.find{ it.id == downloadRequest.id}
+                val downLoadInfo = remember(key1 = workInfos) {
+                    workInfos?.find {
+//                        Log.d("IMAGEURI", it.id.toString())
+
+                        it.id == downloadRequest.id
+                    }
                 }
-                val filterInfo = remember(key1 = workInfos){
-                    workInfos?.find{ it.id == colorFilterRequest.id}
+                val filterInfo = remember(key1 = workInfos) {
+                    workInfos?.find { it.id == colorFilterRequest.id }
                 }
 
-                val imageUri by remember(key1 = downLoadInfo, key2 = filterInfo) {
+                val imageUri by remember(
+                    key1 = downLoadInfo,
+                    key2 = filterInfo
+                ) {
                     derivedStateOf {
                         val downloadUri = downLoadInfo?.outputData?.getString(WorkerKeys.IMAGE_URI)
                             ?.toUri()
@@ -62,7 +88,10 @@ class MainActivity : ComponentActivity() {
                         filterUri ?: downloadUri
                     }
                 }
-                Log.d("IMAGEURI", imageUri.toString())
+                Log.d(
+                    "IMAGEURI",
+                    imageUri.toString()
+                )
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
@@ -78,34 +107,35 @@ class MainActivity : ComponentActivity() {
                         )
                         Spacer(Modifier.height(16.dp))
                     }
-                    Button(onClick = {
-                        workManager.beginUniqueWork(
-                            "download",
-                            ExistingWorkPolicy.KEEP,
-                            downloadRequest
-                        )
-                        .then(colorFilterRequest)
-                        .enqueue()
-                    },
-                    enabled = downLoadInfo?.state != WorkInfo.State.RUNNING
-                        ) {
+                    Button(
+                        onClick = {
+                            workManager.beginUniqueWork(
+                                "download",
+                                ExistingWorkPolicy.KEEP,
+                                downloadRequest
+                            )
+                                .then(colorFilterRequest)
+                                .enqueue()
+                        },
+                        enabled = downLoadInfo?.state != WorkInfo.State.RUNNING
+                    ) {
                         Text(text = "Start download")
                     }
                     Spacer(Modifier.height(8.dp))
-                    when(downLoadInfo?.state){
-                        WorkInfo.State.RUNNING ->  Text(text = "Downloading...")
-                        WorkInfo.State.SUCCEEDED ->  Text(text = "Download succeeded")
-                        WorkInfo.State.FAILED ->  Text(text = "Download cancelled")
-                        WorkInfo.State.ENQUEUED ->  Text(text = "Download enqueed")
-                        WorkInfo.State.BLOCKED ->  Text(text = "Download blocked")
+                    when (downLoadInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text(text = "Downloading...")
+                        WorkInfo.State.SUCCEEDED -> Text(text = "Download succeeded")
+                        WorkInfo.State.FAILED -> Text(text = "Download cancelled")
+                        WorkInfo.State.ENQUEUED -> Text(text = "Download enqueed")
+                        WorkInfo.State.BLOCKED -> Text(text = "Download blocked")
                     }
                     Spacer(Modifier.height(8.dp))
-                    when(filterInfo?.state){
-                        WorkInfo.State.RUNNING ->  Text(text = "Applying filter...")
-                        WorkInfo.State.SUCCEEDED ->  Text(text = "Filter succeeded")
-                        WorkInfo.State.FAILED ->  Text(text = "Filter cancelled")
-                        WorkInfo.State.ENQUEUED ->  Text(text = "Filter enqueed")
-                        WorkInfo.State.BLOCKED ->  Text(text = "Filter blocked")
+                    when (filterInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text(text = "Applying filter...")
+                        WorkInfo.State.SUCCEEDED -> Text(text = "Filter succeeded")
+                        WorkInfo.State.FAILED -> Text(text = "Filter cancelled")
+                        WorkInfo.State.ENQUEUED -> Text(text = "Filter enqueed")
+                        WorkInfo.State.BLOCKED -> Text(text = "Filter blocked")
                     }
                 }
             }
