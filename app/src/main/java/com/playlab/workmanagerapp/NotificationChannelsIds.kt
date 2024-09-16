634ca8846a9000a74cbb6542c08bef6c098d725e
João Pedro <joaoplay16@gmail.com>
2024-09-16 20:10:11 -0300

    Create NotificationChannelIds object
    Hold app notifications channel ids

---
+++ app/src/main/java/com/playlab/workmanagerapp/NotificationChannelsIds.kt
@@ -0,0 +1,7 @@
+package com.playlab.workmanagerapp
+
+object NotificationChannelsIds {
+    const val DOWNLOAD_CHANNEL_ID = "download_channel"
+    const val ALARM_MANAGER_CHANNEL_ID = "alarm_manager_channel"
+    const val JOB_SCHEDULER_CHANNEL_ID = "job_scheduler_channel"
+}
